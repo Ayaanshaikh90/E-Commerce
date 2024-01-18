074a322669d643acbed61ad784190b393a82edd4
Ayaan shaikh <iayaanshaikh.01@gmail.com>
2024-01-18 23:03:48 +0530

    products, newcollection section added

--- frontend-e-commerce/src/Pages/Shop.jsx
+++ frontend-e-commerce/src/Pages/Shop.jsx
@@ -1,10 +1,20 @@
 import React from "react";
 import Hero from "../Components/Hero/Hero";
+import Polular from "../Components/Popular/Polular";
+import Offers from "../Components/Offers/Offers";
+import NewCollections from "../Components/NewCollection/newCollections";
+import NewsLetter from "../Components/NewsLetter/NewsLetter";
+import Footer from "../Components/Footer/Footer";
 
 const Shop = () => {
   return (
     <div>
       <Hero />
+      <Polular />
+      <Offers />
+      <NewCollections />
+      <NewsLetter />
+      <Footer />
     </div>
   );
 };
